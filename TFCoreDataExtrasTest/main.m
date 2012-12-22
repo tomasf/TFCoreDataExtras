6119719dc3cb22c09dfb90752b63e9a7a7590212
Tomas Franzén <tomas@tomasf.se>
2012-12-22 19:39:39 +0100

    ARC

--- TFCoreDataExtrasTest/main.m
+++ TFCoreDataExtrasTest/main.m
@@ -4,17 +4,16 @@
 
 
 int main (int argc, const char * argv[]) {
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	
-	NSManagedObjectContext *moc = [NSManagedObjectContext managedObjectContextFromModelNamed:@"Model" storeName:@"TestStore" type:NSSQLiteStoreType];
-	
-	[[[TFPerson alloc] initWithName:@"Tomas" country:@"Sweden" insertingIntoManagedObjectContext:moc] autorelease];
-	[[[TFPerson alloc] initWithName:@"Pelle" country:@"Sweden" insertingIntoManagedObjectContext:moc] autorelease];
-	[[[TFPerson alloc] initWithName:@"Igor" country:@"Russia" insertingIntoManagedObjectContext:moc] autorelease];
-	
-	NSArray *swedes = [TFPerson objectsInManagedObjectContext:moc sortedBy:@"name" ascending:YES matchingPredicateFormat:@"country = %@", @"Sweden"];
-	NSLog(@"All swedes: %@", [swedes valueForKey:@"name"]);
-
-	[pool release];
-    return 0;
+	@autoreleasepool {
+		
+		NSManagedObjectContext *moc = [NSManagedObjectContext managedObjectContextFromModelNamed:@"Model" storeName:@"TestStore" type:NSSQLiteStoreType];
+		
+		[[[TFPerson alloc] initWithName:@"Tomas" country:@"Sweden" insertingIntoManagedObjectContext:moc] self];
+		[[[TFPerson alloc] initWithName:@"Pelle" country:@"Sweden" insertingIntoManagedObjectContext:moc] self];
+		[[[TFPerson alloc] initWithName:@"Igor" country:@"Russia" insertingIntoManagedObjectContext:moc] self];
+		
+		NSArray *swedes = [TFPerson objectsInManagedObjectContext:moc sortedBy:@"name" ascending:YES matchingPredicateFormat:@"country = %@", @"Sweden"];
+		NSLog(@"All swedes: %@", [swedes valueForKey:@"name"]);
+	}
+	return 0;
 }
